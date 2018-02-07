96c430a161f7b19d10499ff43068c9b943550ac3
asfa164 <30317457+asfa164@users.noreply.github.com>
2018-02-07 03:02:14 -0800

    matrix

---
+++ MatrixLib/MatrixAddSub.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include"stdafx.h"
+using namespace std;
+
+
+int AddMatrix(int** A, int** B, int** C, int colA, int colB, int rowA, int rowB){
+
+	if (colA ==colB && rowB==rowA){
+		 C = new int*[rowA];
+		for (int ii = 0; ii < rowA; ++ii)
+			C[ii] = new int[colA];
+
+		for (int i = 0; i < rowA; i++)
+		{
+			for (int j = 0; j < colA; j++)
+			{
+				
+				
+					C[i][j] =  A[i][j] + B[i][j];
+				
+			}
+		}
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
+
+}
+int SubtractMatrix(int** A, int** B, int** C, int colA, int colB, int rowA, int rowB){
+
+	if (colA == colB && rowB == rowA){
+		C = new int*[rowA];
+		for (int ii = 0; ii < rowA; ++ii)
+			C[ii] = new int[colA];
+
+		for (int i = 0; i < rowA; i++)
+		{
+			for (int j = 0; j < colA; j++)
+			{
+
+
+				C[i][j] = A[i][j] - B[i][j];
+
+			}
+		}
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
+
+}
